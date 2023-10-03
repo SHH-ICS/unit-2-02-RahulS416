f59681ae35eceb34f924a9b826850b24d3a7e94f
RahulS416 <144241896+RahulS416@users.noreply.github.com>
2023-10-03 09:27:00 -0400

    Update program.py

--- program.py
+++ program.py
@@ -1,5 +1,6 @@
 # My Python Program
 # Task:  Use the function myFunction to output a simple "Hello World!" statement
 
-def myFunction():
+def myFunction(string name):
+  return "Hello" name"
   # My Function
