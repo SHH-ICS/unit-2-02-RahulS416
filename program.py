f8ff7bafe6f00dc6dfa1be86e6b3cf12d280818b
RahulS416 <144241896+RahulS416@users.noreply.github.com>
2023-10-03 09:29:40 -0400

    Update program.py
    Extra

--- program.py
+++ program.py
@@ -1,6 +1,8 @@
 # My Python Program
 # Task:  Use the function myFunction to output a simple "Hello World!" statement
 
-def myFunction(string name):
-  return "Hello" name"
+def myFunction(name):
+  return "Hello," + name + "!"
   # My Function
+username = input()
+print(myFunction(username))
